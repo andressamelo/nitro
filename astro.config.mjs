88495758cc2b93905beeb9dc44c621a0a55cc6eb
Andressa Melo <contato@andressamelo.com.br>
2025-06-09 22:10:27 -0300

    add astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,20 @@
 import tailwind from "@astrojs/tailwind";
+import path from 'path';
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
+
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://nitroag.com',
   integrations: [tailwind(), icon()],
+  vite: {
+  resolve: {
+    alias: {
+      '~': path.resolve('./src'),
+    },
+  },
+  },
 });
+
+
